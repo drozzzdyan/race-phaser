3f4ebe70ec2a96efd6c34585dc6e34d3f9f337c9
Daniil <kabzdec333@gmail.com>
2023-11-10 12:49:20 +0300

    create server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const PORT = 3000;
+const DOCROOT = './../dist/';
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+// creating server
+const app = express();
+const server = http.createServer(app);
+
+// return data
+const documentRoot = path.join(__dirname, DOCROOT);
+const staticContent = express.static(documentRoot);
+app.use(staticContent);
+
+// starting server
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+})
